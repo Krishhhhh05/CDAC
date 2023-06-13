6511ebf36176e471454459c2a6bf8f14ef0b0ac3
MyEngineeringLife <124250994+MyEngineeringLife@users.noreply.github.com>
2023-06-13 23:21:47 +0530

    13/06 23:21

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Nav from 'react-bootstrap/Nav';
 
 const Home = () => {
   return (
@@ -8,22 +7,6 @@ const Home = () => {
     <div class="gap-x-7 mx-6 gap-9">
 
 
-
-    <Nav variant="tabs" defaultActiveKey="/home">
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-  
-export default TabsExample;
 
       <Link to="/region">
       <button type="button" class="btn btn-primary mx-6 my-2">Region</button>    
