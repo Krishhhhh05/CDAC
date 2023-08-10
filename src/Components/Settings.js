b226fb958f4ab5c82e06ec8c213bf8561423df36
candelatesla <candelatesla@gmail.com>
2023-08-11 01:23:42 +0530

    edits5

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -19,48 +19,47 @@ class SettingsPage extends React.Component {
     render() {
         return (
             <>
+                <Sidebar />
                 <div className='App'>
-                    <div className="settings-page">
-                        <Sidebar />
-                        <h1>Student Settings</h1>
-                        <div className="profile-photo">
-                            <img src="../assets/theorylec2.png" alt="Profile" style={{ width: '40%', height: '40%', }} />
-                            <button>Change Photo</button>
-                        </div>
-                        <div>
-                            <label>Name:</label>
-                            <input
-                                type="text"
-                                value={this.state.name}
-                                onChange={(e) => this.setState({ name: e.target.value })}
-                            />
-                        </div>
-                        <div>
-                            <label>SAP ID:</label>
-                            <input
-                                type="text"
-                                value={this.state.sapId}
-                                onChange={(e) => this.setState({ sapId: e.target.value })}
-                            />
-                        </div>
-                        <div>
-                            <label>College:</label>
-                            <input
-                                type="text"
-                                value={this.state.college}
-                                onChange={(e) => this.setState({ college: e.target.value })}
-                            />
-                        </div>
-                        <div>
-                            <h2>Test Results</h2>
-                            <ul>
-                                {this.state.testResults.map((result, index) => (
-                                    <li key={index}>
-                                        {result.subject}: {result.score}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+
+                    <h1>Student Settings</h1>
+                    <div>
+                        <img src="../assets/theorylec2.png" alt="Profile" style={{ width: '40%', height: '40%', align: 'center', }} />
+                        <button>Change Photo</button>
+                    </div>
+                    <div>
+                        <label>Name:</label>
+                        <input
+                            type="text"
+                            value={this.state.name}
+                            onChange={(e) => this.setState({ name: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <label>SAP ID:</label>
+                        <input
+                            type="text"
+                            value={this.state.sapId}
+                            onChange={(e) => this.setState({ sapId: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <label>College:</label>
+                        <input
+                            type="text"
+                            value={this.state.college}
+                            onChange={(e) => this.setState({ college: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <h2>Test Results</h2>
+                        <ul>
+                            {this.state.testResults.map((result, index) => (
+                                <li key={index}>
+                                    {result.subject}: {result.score}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </>
