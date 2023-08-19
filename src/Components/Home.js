a141c4c82d837b13c82f2eb43a2e8e8a5a749ab0
candelatesla <candelatesla@gmail.com>
2023-08-19 16:08:21 +0530

    ydkbedits

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,100 +11,97 @@ import Sidebar from './Sidebar';
 function Home() {
   return (
     <>
-      {/* <div>
-        <h1>My React App</h1>
-        <Anime />
-      </div> */}
-      <div className='App'>
-        < div class="flex justify-start items-center h-screen">
-          <Sidebar />
-          <div className="card-container mx-5">
-            {/* <div className="card">
-              <h4>Hello Student!</h4>
-              <p>It is good to see you again.</p>
-            </div>
-            <div className="card">
-              <h4>Time to learn inequalities in graph</h4>
-              <p>Follow along the lectures and test yourself.</p>
-            </div> */}
 
-            <h1> Lectures </h1>
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <h6>Explaining Inequations</h6>
-              <Link to="/theory1">
-                <button type="button" class="btn btn-primary mx-6 my-2 ">View lecture</button>
-              </Link>
-            </div>
+      <div className='App h-full'>
 
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <h6>Understanding Planes of Axis</h6>
-              <Link to="/theory2">
-                <button type="button" class="btn btn-primary mx-6 ">View lecture</button>
-              </Link>
-            </div>
+        <div className='grid grid-cols-12 gap-4'>
+          <div className='col-span-2 flex items-center ml-7'>
+            <Sidebar />
+          </div>
+          <div className='col-span-5 flex items-center'>
 
-            <div className="card flex flex-row text-center font-semibold mt-3 py- items-center justify-center">
-              <h6>Plotting of Lines</h6>
-              <Link to="/theory3">
-                <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 ">View lecture</button>
-              </Link>
-            </div>
+            <div className="">
+              <h3> LECTURES</h3>
+              <div className="card flex flex-row justify-between text-center font-semibold mt-3 py-3 px-16 items-center">
+                <h6 className='ml-6'>Explaining Inequations</h6>
+                <Link to="/theory1">
+                  <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 ">View lecture</button>
+                </Link>
+              </div>
 
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <h6>Shading of Region</h6>
-              <Link to="/theory4">
-                <button type="button" class="btn btn-primary mx-6 my-2 ">View lecture</button>
-              </Link>
+              <div className="card flex flex-row justify-between text-center font-semibold mt-3 py-3 px-16 items-center">
+                <h6 className='ml-6'>Understanding Planes of Axis</h6>
+                <Link to="/theory2">
+                  <button type="button" class="btn btn-primary mx-6 ">View lecture</button>
+                </Link>
+              </div>
+
+              <div className="card flex flex-row justify-between text-center font-semibold mt-3 py-3 px-16 items-center">
+                <h6 className='ml-6'> Plotting of Lines</h6>
+                <Link to="/theory3">
+                  <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 ">View lecture</button>
+                </Link>
+              </div>
+
+              <div className="card flex flex-row justify-between text-center font-semibold mt-3 py-3 px-16 items-center">
+                <h6 className='ml-6'>Shading of Region</h6>
+                <Link to="/theory4">
+                  <button type="button" class="btn btn-primary mx-6 my-2 ">View lecture</button>
+                </Link>
+              </div>
             </div>
           </div>
 
-          <div className="card-container mx-5">
-            <h1>Games</h1>
+          <div className='col-span-5 flex items-center h-screen'>
+            <div className="card-container mx-5">
+              <h3>GAMES</h3>
 
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <MdAutoGraph className="h-8 w-8 text-gray-500 float-left mr-3" />
+              <div className="card flex flex-row justify-between text-center font-semibold my-2 py-3 px-8 items-center">
+                <div className="flex items-center">
+                  <MdAutoGraph className="h-8 w-8 text-gray-500 mr-3" />
+                  <h6>Plot The Graph</h6>
+                </div>
+                <Link to="/plot">
+                  <button type="button" className="btn btn-primary py-2 px-6 h-10 w-20">Try</button>
+                </Link>
+              </div>
 
-              <h6>Plot The Graph</h6>
+              <div className="card flex flex-row justify-between text-center font-semibold my-2 py-3 px-8 items-center">
+                <div className="flex items-center">
+                  <AiOutlineArrowsAlt className="h-8 w-8 text-gray-500 mr-3" />
+                  <h6>Points And Intercepts</h6>
+                </div>
+                <Link to="/points">
+                  <button type="button" className="btn btn-primary py-2 px-6 h-10 w-20">Try</button>
+                </Link>
+              </div>
 
-              <Link to="/plot">
-                <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 h-10 w-20">Try</button>
-              </Link>
+              <div className="card flex flex-row justify-between text-center font-semibold my-2 py-3 px-8 items-center">
+                <div className="flex items-center">
+                  <FaChartArea className="h-8 w-8 text-gray-500 float-left mr-3" />
+                  <h6>Lines Game</h6>
+                </div>
+                <Link to="/multi">
+                  <button type="button" className="btn btn-primary py-2 px-6 h-10 w-20">Play</button>
+                </Link>
+              </div>
+
+              <div className="card flex flex-row justify-between text-center font-semibold my-2 py-3 px-8 items-center">
+                <div className="flex items-center">
+                  <LuAxis3D className="h-8 w-8 text-gray-500 mr-3" />
+                  <h6>Region Game</h6>
+                </div>
+                <Link to="/region">
+                  <button type="button" className="btn btn-primary py-2 px-6 h-10 w-20">Play</button>
+                </Link>
+              </div>
+
+              <div className="bg-black rounded-md flex flex-row justify-center text-center font-semibold my-2 py-4 px-8 items-center">
+                <Link to="/quiz">
+                  <button className="bg-white text-black py-2 px-10 h-12 rounded-md">GET TESTED</button>
+                </Link>
+              </div>
             </div>
-
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <AiOutlineArrowsAlt className="h-8 w-8 text-gray-500 float-left mr-3" />
-              <h6>Points And Intercepts</h6>
-              <Link to="/points">
-                <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 h-10 w-20">Try</button>
-              </Link>
-            </div>
-
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <FaChartArea className="h-8 w-8 text-gray-500 float-left mr-3" />
-              <h6>Lines Game</h6>
-              <Link to="/multi">
-                <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 h-10 w-20">Play</button>
-              </Link>
-            </div>
-
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <LuAxis3D className="h-8 w-8 text-gray-500 float-left mr-3" />
-              <h6>Region Game</h6>
-              <Link to="/region">
-                <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 h-10 w-20">Play</button>
-              </Link>
-            </div>
-
-            <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <h2>Get Tested</h2>
-              <Link to="/quiz">
-                <button type="button" class="btn btn-primary mx-6 my-2 py-2 px-10 h-10 w-20">Begin</button>
-              </Link>
-            </div>
-
-            {/* <div className="card flex flex-row text-center font-semibold mt-3 py-3 items-center justify-center">
-              <h2>Ask Doubts</h2>
-            </div> */}
           </div>
         </div>
       </div>
--- src/Components/TheoryLec3.js
+++ src/Components/TheoryLec3.js
@@ -1,106 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { AiFillHome } from 'react-icons/ai';
-import { IoLogoAndroid } from 'react-icons/io';
-import { FaGraduationCap } from 'react-icons/fa';
-import { IoLogoGameControllerB } from 'react-icons/io';
-import { BsPencilFill } from 'react-icons/bs';
-import { IoMdSettings } from 'react-icons/io';
-import { IoMdExit } from 'react-icons/io';
-import Swal from 'sweetalert2';
+
+import './Style.css';
+import Sidebar from './Sidebar';
 
 
 const TheoryLec3 = () => {
-  const handleExit = () => {
-    Swal.fire({
-      title: 'Are you sure you want to exit?',
-      showCancelButton: true,
-      confirmButtonText: 'No',
-      cancelButtonText: 'Yes, exit',
-      reverseButtons: true,
-    }).then((result) => {
-      if (result.dismiss === Swal.DismissReason.cancel) {
-        window.close();
-      }
-    });
-  };
+
+
+
   return (
     <>
-      <div className='App'>
-        < div class="flex m-2 justify-start items-center h-screen">
-          <div id="sidebar" class="bg-black rounded-lg h-90 w-20 flex flex-col items-center">
-            <div class="text-white p-8">
-              <div>
-                <IoLogoAndroid className="h-12 w-12 text-gray-500" />
-              </div>
+      <div className=''>
+        <div className='grid grid-cols-12 gap-4'>
+          <div className='col-span-1 flex items-center ml-7'>
+
+            <Sidebar />
+          </div>
+          <div className='col-span-11'>
+            <div className="flex justify-center">
+              <h6 className="mb-2 text-2xl font-bold">PLOTTING OF LINES</h6>
             </div>
-            <div class="flex flex-col flex-grow justify-center">
-              <div class="text-white p-3">
-                <div>
-                  <Link to="/" title="Home">
-                    <AiFillHome className="h-6 w-6 text-gray-500" />
-                  </Link>
-                </div>
-              </div>
-              <div class="text-white p-3">
-                <div>
-                  <Link to="/theory1" title="Lecture">
-                    <FaGraduationCap className="h-6 w-6 text-gray-500" />
-                  </Link>
 
-                </div>
-              </div>
-              <div class="text-white p-3">
-                <div>
-                  <Link to="/multi" title="Games">
-                    <IoLogoGameControllerB className="h-6 w-6 text-gray-500" />
-                  </Link>
+            <div class="h-full flex items-center w-5/6 ml-20">
+              <div class="max-h-[500px] overflow-y-auto bg-white p-6">
 
-                </div>
-              </div>
-              <div class="text-white p-3">
-                <div>
-                  <Link to="/quiz" title="Test">
-                    <BsPencilFill className="h-6 w-6 text-gray-500" />
-                  </Link>
 
-                </div>
-              </div>
-              <div class="text-white p-3">
-                <div>
-                  <Link to="/" title="Settings">
-                    <IoMdSettings className="h-6 w-6 text-gray-500" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div class="text-white p-16">
-              <div>
-                <button onClick={handleExit} title="Exit">
-                  <IoMdExit className="h-8 w-8 text-gray-500" />
-                </button>
+                <article>
+                  <p>To plot a line in slope-intercept form (y = mx + b), you can follow these steps:
+
+                    Identify the y-intercept: The y-intercept is the point where the line intersects the y-axis. It is represented by the value of b in the equation y = mx + b. If the equation is not in slope-intercept form, solve for y to get it in that form.
+
+                    Plot the y-intercept: Plot a point on the y-axis that corresponds to the value of b.
+
+                    Determine the slope: The slope of the line is represented by the value of m in the equation y = mx + b. The slope tells you how steep the line is and in which direction it is moving. If the slope is positive, the line will move up and to the right. If the slope is negative, the line will move down and to the right. If the slope is zero, the line will be horizontal.
+
+                    Use the slope to find another point: Starting from the y-intercept, move along the x-axis to the right by the value of the denominator of the slope. Then, move up or down along the y-axis by the value of the numerator of the slope. This will give you another point on the line.
+
+                    Plot the second point: Plot the second point on the graph.
+
+                    Draw the line: Use a ruler or a straightedge to draw a line that passes through both points.</p>
+                </article>
               </div>
             </div>
           </div>
-          <section>
-            <center><h2>Plotting of Lines</h2></center>
-            <article>
-              <p>To plot a line in slope-intercept form (y = mx + b), you can follow these steps:
-
-                Identify the y-intercept: The y-intercept is the point where the line intersects the y-axis. It is represented by the value of b in the equation y = mx + b. If the equation is not in slope-intercept form, solve for y to get it in that form.
-
-                Plot the y-intercept: Plot a point on the y-axis that corresponds to the value of b.
-
-                Determine the slope: The slope of the line is represented by the value of m in the equation y = mx + b. The slope tells you how steep the line is and in which direction it is moving. If the slope is positive, the line will move up and to the right. If the slope is negative, the line will move down and to the right. If the slope is zero, the line will be horizontal.
-
-                Use the slope to find another point: Starting from the y-intercept, move along the x-axis to the right by the value of the denominator of the slope. Then, move up or down along the y-axis by the value of the numerator of the slope. This will give you another point on the line.
-
-                Plot the second point: Plot the second point on the graph.
-
-                Draw the line: Use a ruler or a straightedge to draw a line that passes through both points.</p>
-            </article>
-            <center><img src="../assets/theorylec3.jpg" alt="Plotting of Lines" style={{ width: '40%', height: '40%', }}></img></center>
-          </section>
         </div>
       </div>
     </>
